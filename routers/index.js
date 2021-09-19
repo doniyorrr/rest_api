c30d4e0184ca339bf364694c0e63e073b472f831
doniyor <elmurodovdoniyor589@gmail.com>
2021-09-19 14:40:36 +0500

    api

--- routers/index.js
+++ routers/index.js
@@ -35,6 +35,74 @@ router.post('/' , (req , res)=>{
         console.log(err)
     })
 })
+//  | Get a movie. 
+router.get('/:movie_id' , (req , res)=>{
+    Schema.findById(req.params.movie_id)
+   .then((data)=>{
+       res.json(data)
+   })
+   .catch((err)=>{
+       console.log(err)
+   })
+})
+
+// | Update a movie with new info. |
+router.put('/:movie_id' , (req , res)=>{
+    Schema.findByIdAndUpdate(req.params.movie_id , req.body)
+   .then((data)=>{
+       res.json(data)
+   })
+   .catch((err)=>{
+       console.log(err)
+   })
+})
+
+// | Delete a movie. |
+router.delete('/:movie_id' , (req , res)=>{
+    Schema.findByIdAndRemove(req.params.movie_id )
+   .then((data)=>{
+       res.json(data)
+   })
+   .catch((err)=>{
+       console.log(err)
+   })
+})
+
+// |Get the top 10 movies. |
+router.get('/top/top10' , (req , res)=>{
+    Schema.find({}).limit(10).sort({imdb_score: -1 })
+   .then((data)=>{
+       res.json(data)
+   })
+   .catch((err)=>{
+       console.log(err)
+   })
+}) 
+
+// | Movies between two dates. |
+router.get('/:start_year/:end_year' , (req , res)=>{
+    const {start_year , end_year} = req.params
+    Schema.find({
+        year:{'$gte':parseInt(start_year) , "$lte":parseInt(end_year)}
+    })
+   .then((data)=>{
+       res.json(data)
+   })
+   .catch((err)=>{
+       console.log(err)
+   })
+})
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router
 
